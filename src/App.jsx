d8e9e6c148d37f27ecbe447d07c1647952cc93bc
DEvARyan77 <devaryan7766@gmail.com>
2025-04-18 02:06:03 +0530

    Login Signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Meet from './components/studentMeet'
 import Home from './pages/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/meet" element={<Meet />} />
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   )
