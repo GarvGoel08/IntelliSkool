3a63d8554d85a56f9772cd62caf300ee9c296f04
DEvARyan77 <devaryan7766@gmail.com>
2025-04-18 04:01:24 +0530

    Create class backend remaining

--- src/components/CreateClass.jsx
+++ src/components/CreateClass.jsx
@@ -27,11 +27,22 @@ function CreateClass() {
                         className={`transition-all duration-500 overflow-hidden items-center py-4 px-2 border-t text-xl font-semibold cursor-pointer border-gray-300 ${showCreateForm ? 'h-[calc(100vh-120px)] opacity-100 p-4' : 'h-[60px] opacity-100 p-4'}`}
                     >
                         <p className='cursor-pointer'>Create Classroom</p><br />
-                        <input placeholder='Classroom Title' className="border border-gray-300 px-4 py-2 rounded-md outline-none" />
-                        <input placeholder='Classroom Description' className="border border-gray-300 px-4 py-2 rounded-md outline-none" />
-                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition">
-                            Save
-                        </button>
+                        <div className='flex flex-col w-[450px]'>
+                            <input
+                                placeholder='Classroom Title'
+                                className="border-b-2 border-[#2E3A59] focus:border-blue-500 px-4 py-2 outline-none transition-colors duration-200"
+                            /><br />
+                            <textarea
+                                placeholder='Classroom Description'
+                                className="border overflow-auto h-[150px] border-[#2E3A59] focus:border-blue-500 px-2 py-2 outline-none transition-colors duration-200 resize-none" style={{
+                                    scrollbarWidth: 'none',  /* Firefox */
+                                    msOverflowStyle: 'none'  /* Internet Explorer */
+                                }}
+                            /><br />
+                            <div className="bg-blue-0 border flex items-center justify-center text-[#2E3A59] px-4 rounded-xl py-2 w-[100px] hover:bg-[#2E3A59] hover:text-white transition-colors duration-200">
+                                Save
+                            </div>
+                        </div>
                     </div>
 
                 </div>
